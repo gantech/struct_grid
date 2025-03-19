32d17f55eee2c7cf9da45bfca449f5af19a6fc01
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-03-19 13:19:59 -0600

    Implement simple multigrid

---
+++ src/multigrid.cu
@@ -0,0 +1,276 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <cuda_runtime.h>
+#include <thrust/reduce.h>
+#include <thrust/device_vector.h>
+
+
+// Kernel function for initialization - No tiling or shared memory
+__global__ void initialize(double *T, int nx, int ny, double dx, double dy);
+
+// Kernel function for update - No tiling or shared memory
+__global__ void update(double *T, double *deltaT, int nx, int ny, double dx, double dy);
+
+// Kernel function for calculation of Jacobian and Residual - No tiling or shared memory
+__global__ void compute_r_j(double *T, double *J, double *R, int nx, int ny, double dx, double dy, double kc);
+
+// Kernel function for calculation of Residual - No tiling or shared memory
+__global__ void compute_r(double *T, double * J, double *R, int nx, int ny, double dx, double dy, double kc) ;
+
+
+// Kernel to compute linear residual of the linear system of equations J * deltaT = rhs. 
+// Write linear residual to new array R. If you want the rhs overwritten, pass the same pointers for rhs and R
+__global__ void compute_lin_resid(double * deltaT, double * J, double * rhs, double * R, int nx, int ny) {
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int idx_r = (j * nx) + i;
+    int idx_j = idx_r * 5;
+
+    if ( (i < nx) && (j < ny)) {
+
+        double jij = J[idx_j];
+        double jim1j = J[idx_j + 1];
+        double jip1j = J[idx_j + 2];
+        double jijm1 = J[idx_j + 3];
+        double jijp1 = J[idx_j + 4];
+
+        double tip1j = 0.0;
+        double tim1j = 0.0;
+        double tijp1 = 0.0;
+        double tijm1 = 0.0;
+
+        if ( i == 0) {
+            tip1j = deltaT[idx_r + 1];
+        } else if ( i == (nx - 1)) {
+            tim1j = deltaT[idx_r - 1];
+        } else {
+            tip1j = deltaT[idx_r + 1];
+            tim1j = deltaT[idx_r - 1];
+        }
+
+        if ( j == 0) {
+            tijp1 = deltaT[idx_r + nx];
+        } else if ( j == (ny - 1)) {
+            tijm1 = deltaT[idx_r - nx];
+        } else {
+            tijm1 = deltaT[idx_r - nx];
+            tijp1 = deltaT[idx_r + nx];
+        }
+
+        // Write to residual
+        R[idx_r] = rhs[idx_r] - jim1j * tim1j - jip1j * tip1j - jijm1 * tijm1 - jijp1 * tijp1 - jij * deltaT[idx_r];
+    }
+}
+
+// Restrict residual by one level. Expected that nxc = nxf/2 and nyc = nyf/2
+// Expected to run on a grid and block that represents the coarse mesh
+__global__ restrict_resid(double * rc, double * rf, int nxc, int nyc, int nxf, int nyf) {
+
+    int ic = blockIdx.x * blockDim.x + threadIdx.x;
+    int jc = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // Each cell in the coarse mesh (ic, jc) is a sum of the 4 cells corresponding to (2 * ic, 2 * jc), (2 * ic + 1, 2 * jc), (2 * ic, 2 * jc + 1), (2 * ic + 1, 2 * jc + 1)
+    int idx_rc = (jc * nxc) + i;
+    int idx_rf1 = (2 * jc * nxf) + (2 * ic);
+    int idx_rf2 = (2 * jc * nxf) + (2 * ic + 1);
+    int idx_rf3 = (2 * (jc + 1) * nxf) + (2 * ic);
+    int idx_rf4 = (2 * (jc + 1) * nxf) + (2 * ic + 1);
+
+    if ( (i < nxc) && (j < nyc) ) {
+
+        rc[idx_rc] = rf[idx_rf1] + rf[idx_rf2] + rf[idx_rf3] + rf[idx_rf4];
+        
+    }
+}
+
+// Prolongate error by one level. Expected that nxc = nxf/2 and nyc = nyf/2
+// Expected to run on a grid and block that represents the coarse mesh
+__global__ prolongate_error(double * deltaTc, deltaTf, int nxc, int nyc, int nxf, int nyf) {
+
+    int ic = blockIdx.x * blockDim.x + threadIdx.x;
+    int jc = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // Each cell in the coarse mesh (ic, jc) is a sum of the 4 cells corresponding to (2 * ic, 2 * jc), (2 * ic + 1, 2 * jc), (2 * ic, 2 * jc + 1), (2 * ic + 1, 2 * jc + 1)
+    int idx_rc = (jc * nxc) + i;
+    int idx_rf1 = (2 * jc * nxf) + (2 * ic);
+    int idx_rf2 = (2 * jc * nxf) + (2 * ic + 1);
+    int idx_rf3 = (2 * (jc + 1) * nxf) + (2 * ic);
+    int idx_rf4 = (2 * (jc + 1) * nxf) + (2 * ic + 1);
+
+    if ( (i < nxc) && (j < nyc) ) {
+
+        deltaTf[idx_rf1] += deltaTc[idx_rc];
+        deltaTf[idx_rf2] += deltaTc[idx_rc];
+        deltaTf[idx_rf3] += deltaTc[idx_rc];
+        deltaTf[idx_rf4] += deltaTc[idx_rc];
+        
+    }    
+
+}
+
+
+// Create Jacobian matrix at the coarser level by using the finer level. Expected that nxc = nxf/2 and nyc = nyf/2
+// Expected to run on a grid and block that represents the coarse mesh
+__global__ void restrict_J(double * jc, double * jf, int nxc, int nyc, int nxf, int nyf)  {
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // Each cell in the coarse mesh (ic, jc) is a sum of the 4 cells corresponding to (2 * ic, 2 * jc), (2 * ic + 1, 2 * jc), (2 * ic, 2 * jc + 1), (2 * ic + 1, 2 * jc + 1)
+    int idx_jc = ((j * nxc) + i) * 5;
+    int idx_jf1 = ((2 * j * nxf) + (2 * i)) * 5;
+    int idx_jf2 = ((2 * j * nxf) + (2 * i + 1)) * 5;
+    int idx_jf3 = ((2 * (j + 1) * nxf) + (2 * i)) * 5;
+    int idx_jf4 = ((2 * (j + 1) * nxf) + (2 * i + 1)) * 5;
+
+    if ( (i < nxc) && (j < nyc) ) {
+
+        jc[idx_jc] = jf[idx_jf1] + jf[idx_jf2] + jf[idx_jf3] + jf[idx_jf4]  // Diagonals of the 4 cells
+                    + jf[idx_jf1+2] + jf[idx_jf1+4] 
+                    + jf[idx_jf2+1] + jf[idx_jf2+4]
+                    + jf[idx_jf3+3] + jf[idx_jf3+2]
+                    + jf[idx_jf4+4] + jf[idx_jf4+3]; // Interlinks of the 4 cells
+        
+        jc[idx_jc+1] = jf[idx_jf1+1] + jf[idx_jf3+1];
+        jc[idx_jc+2] = jf[idx_jf2+2] + jf[idx_jf4+2];
+        jc[idx_jc+3] = jf[idx_jf1+3] + jf[idx_jf2+3];
+        jc[idx_jc+4] = jf[idx_jf3+4] + jf[idx_jf4+4];
+
+    }
+
+}
+
+// Kernel function for Gauss-Seidel smoother - No tiling or shared memory
+__global__ void gauss_seidel(double *deltaT, double *J, double *R, int nx, int ny) {
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (( i < nx) && (j < ny)) {
+        
+        int idx_r = (j * nx) + i;
+        int idx_j = idx_r * 5;
+
+        double jij = J[idx_j];
+        double jim1j = J[idx_j + 1];
+        double jip1j = J[idx_j + 2];
+        double jijm1 = J[idx_j + 3];
+        double jijp1 = J[idx_j + 4];
+
+        double tip1j = 0.0;
+        double tim1j = 0.0;
+        double tijp1 = 0.0;
+        double tijm1 = 0.0;
+
+        if (row == 0) {
+            tip1j = deltaT[idx_r + 1];
+        } else if (row == (nx - 1)) {
+            tim1j = deltaT[idx_r - 1];
+        } else {
+            tip1j = deltaT[idx_r + 1];
+            tim1j = deltaT[idx_r - 1];
+        }
+
+        if (col == 0) {
+            tijp1 = deltaT[idx_r + nx];
+        } else if (col == (ny - 1)) {
+            tijm1 = deltaT[idx_r - nx];
+        } else {
+            tijm1 = deltaT[idx_r - nx];
+            tijp1 = deltaT[idx_r + nx];
+        }
+
+        deltaT[idx_r] = 0.9*(R[idx_r] - jim1j * tim1j - jip1j * tip1j - jijm1 * tijm1 - jijp1 * tijp1) / jij;
+    }
+}
+
+
+int main() {
+
+    // Finest level problem size
+    int nx_f = 128;
+    int ny_f = 384;
+
+    // Need resolution only on the finest grid to assemble the equations
+    double dx = 1.0 / double(nx_f);
+    double dy = 3.0 / double(ny_f);
+
+    // Number of levels in multigrid - each refined in all directions by a factor of 2
+    int nlevels = 4; 
+    std::vector<int> nx(nlevels);
+    std::vector<int> ny(nlevels);
+    for (int i = 0; i < nlevels; i++) {
+        nx[i] = nx_f / (1 << i);
+        ny[i] = ny_f / (1 << i);
+    }
+
+    // Fields for temperature and non-linear residual are required only at the finest level
+    double * T;
+    cudaMalloc(&T, nx_f * ny_f * sizeof(double));
+    double * nlr;
+    cudaMalloc(&nlr, nx_f * ny_f * sizeof(double));
+
+
+    std::vector<double*> deltaT(nlevels), J(nlevels), R(nlevels);;
+    for (int i = 0; i < nlevels; i++) {
+        cudaMalloc(&deltaT[i], nx[i] * ny[i] * sizeof(double));
+        cudaMalloc(&J[i], nx[i] * ny[i] * sizeof(double));
+        cudaMalloc(&R[i], nx[i] * ny[i] * sizeof(double));
+    }
+
+    // Grid and block size
+    dim3 grid_size(ceil(nx[0] / (double)TILE_SIZE), ceil(ny[0] / (double)TILE_SIZE), 1);
+    for (int ilevel = 0; ilevel < nlevels; ilevel++) 
+        grid_size.push_back((ceil(nx[ilevel] / (double)TILE_SIZE), ceil(ny[ilevel] / (double)TILE_SIZE), 1));
+    
+    // Keep block size same for all grids for now
+    dim3 block_size(TILE_SIZE, TILE_SIZE, 1);
+    std::cout << "Grid size: " << grid_size.x << ", " << grid_size.y << std::endl;
+    std::cout << "Block size: " << block_size.x << ", " << block_size.y << std::endl;
+
+    initialize<<<grid_size[0], block_size>>>(T, nx[0], ny[0], dx, dy);
+
+    // Write 1 V-cycle of multigrid
+    compute_r_j<<<grid_size[0], block_size>>>(T, J[0], nlr, nx[0], ny[0], dx, dy);
+
+    // Compute the Jacobian matrix at the coarser levels 
+    for (int ilevel = 1; ilevel < nlevels; ilevel++)
+        restrict_j<<<grid_size[ilevel], block_size>>>(J[ilevel], J[ilevel-1], nx[ilevel], ny[ilevel], nx[ilevel-1], ny[ilevel-1]);
+    
+    // Do some smoothing on the finest level first
+    for (int ismooth = 0; ismooth < 10; ismooth++)
+        gauss_seidel<<<grid_size[0], block_size>>>(deltaT[0], J[0], nlr[0], nx[0], ny[0]);
+
+    // Compute the residual of the linear system of equations at this level
+    compute_lin_resid<<<grid_size[0], block_size>>>(deltaT[0], J[0], nlr, R[0], nx[0], ny[0]);
+
+    // Downstroke of V-cycle
+    for (int ilevel = 1; ilevel < nlevels; ilevel++) {
+        // Restrict the residual of the linear system
+        restrict_resid<<<grid_size[ilevel], block_size>>>(R[ilevel], R[ilevel-1], nx[ilevel], ny[ilevel], nx[ilevel-1], ny[ilevel-1]);
+
+        // Perform some smoothing at this level to get the error
+        for (int ismooth = 0; ismooth < 10; ismooth++)
+            gauss_seidel<<<grid_size[0], block_size>>>(deltaT[ilevel], J[ilevel], R[ilevel], nx[ilevel], ny[ilevel]);
+
+        // Compute the residual of the linear system of equations at this level. Overwrite the R vector
+        compute_lin_resid<<<grid_size[ilevel], block_size>>>(deltaT[ilevel], J[ilevel], R[ilevel], R[ilevel], nx[ilevel], ny[ilevel]);
+    }
+
+    // Upstroke of V-cycle - This should end on the finest level (ilevel = 0)
+    for (int ilevel = nlevels - 2; ilevel > -1; ilevel--) {
+        // Prolongate the error
+        prolongate_error<<<grid_size[ilevel], block_size>>>(deltaT[ilevel+1], deltaT[ilevel], nx[ilevel+1], ny[ilevel+1], nx[ilevel], ny[ilevel]);
+
+        // Do some more smoothing at this level to reduce the error
+        for (int ismooth = 0; ismooth < 10; ismooth++)
+            gauss_seidel<<<grid_size[0], block_size>>>(deltaT[ilevel], J[ilevel], R[ilevel], nx[ilevel], ny[ilevel]);
+
+    }
+
+    return 0;
+}
