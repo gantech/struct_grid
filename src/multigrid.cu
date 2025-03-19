2f6c8d9a7728be9b988fa92c8934b3b8bf5e8cd0
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-03-19 15:45:12 -0600

    Debug

--- src/multigrid.cu
+++ src/multigrid.cu
@@ -193,6 +193,9 @@ __global__ void gauss_seidel(double *deltaT, double *J, double *R, int nx, int n
         }
 
         deltaT[idx_r] = 0.5*(R[idx_r] - jim1j * tim1j - jip1j * tip1j - jijm1 * tijm1 - jijp1 * tijp1) / jij;
+
+        if (std::isinf(deltaT[idx_r]) || std::isnan(deltaT[idx_r]))
+            printf("i = %d, j = %d, deltaT = %e, R = %e, jim1j = %e, jip1j = %e, jijm1 = %e, jijp1 = %e, jij = %e \n", i, j, deltaT[idx_r], R[idx_r], jim1j, jip1j, jijm1, jijp1, jij);
     }
 }
 
