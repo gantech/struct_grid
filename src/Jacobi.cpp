b7b35553272d48f6bbc8bb5df61bd24d7a978a45
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-03-24 20:22:02 -0600

    Bug

--- src/Jacobi.cpp
+++ src/Jacobi.cpp
@@ -2,7 +2,7 @@
 
 namespace Jacobi {
 
-    void Jacobi(int nx, int ny, double * J, double *T, double *deltaT, double *R):
+    Jacobi(int nx, int ny, double * J, double *T, double *deltaT, double *R):
     LinearSolvers(nx, ny, J, T, deltaT, R)
     {
         cudaMalloc(&deltaT1, nx * ny * sizeof(double));
@@ -14,12 +14,11 @@ namespace Jacobi {
         cudaFree(deltaT1);
     }
 
-
     __host__ void Jacobi::solve_step() {
 
-        Jacobi::jacobi<<<grid_size, block_size>>>(deltaT, deltaT1, J, R, nx, ny);
+        Jacobi::jacobi_kernel<<<grid_size, block_size>>>(deltaT, deltaT1, J, R, nx, ny);
         cudaDeviceSynchronize();
-        Jacobi::jacobi<<<grid_size, block_size>>>(deltaT1, deltaT, J, R, nx, ny);
+        Jacobi::jacobi_kernel<<<grid_size, block_size>>>(deltaT1, deltaT, J, R, nx, ny);
         cudaDeviceSynchronize();                
     }
 
